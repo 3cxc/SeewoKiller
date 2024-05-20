fbcc49d1cfba773037fe7e527ede4cf8e6023d61
3cxc <81449257+3cxc@users.noreply.github.com>
2024-05-20 22:39:29 +0800

    项目的C++核心支持库

---
+++ SeewoCore/pch.cpp
@@ -0,0 +1,66 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+
+#include <Psapi.h>
+#include <string>
+#include <tlhelp32.h>
+#include <conio.h>
+
+using namespace std;
+
+// 用于检查指定进程是否有窗口，如果窗口不可见则返回false
+bool HasWindow(DWORD processID) {
+    HWND hwnd = NULL;
+    do {
+        hwnd = FindWindowEx(NULL, hwnd, NULL, NULL);
+        DWORD windowProcessID = 0;
+        GetWindowThreadProcessId(hwnd, &windowProcessID);
+        if (windowProcessID == processID) {
+            if (IsWindowVisible(hwnd)) {
+                // 检查窗口是否可见
+                return true;
+            }
+        }
+    } while (hwnd != NULL);
+    return false;
+}
+
+// 检查指定的进程是否在前台
+bool IsProcessForeground(DWORD processId) {
+    HWND hWnd = GetForegroundWindow();
+    DWORD foregroundProcessId;
+    GetWindowThreadProcessId(hWnd, &foregroundProcessId);
+    return foregroundProcessId == processId;
+}
+
+// 杀死进程的函数
+bool KillProcess(DWORD processID) {
+    HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, processID);
+    if (hProcess != NULL) {
+        TerminateProcess(hProcess, 0);
+        CloseHandle(hProcess);
+        return true;
+    }
+    return false;
+}
+
+// 遍历系统中符合指定名称的进程并返回它们的进程ID
+vector<DWORD> TraversalProcesses(const wchar_t* targetProcessName) {
+    vector<DWORD> processIDs; // 存储所有同名进程的ID
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);//获取系统中所有正在运行的进程
+    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (Process32First(hProcessSnap, &pe32)) {
+        //遍历检测进程，将名称符合的进程的ID存储到processIDs中
+        do {
+            if (wcscmp(pe32.szExeFile, targetProcessName) == 0) {
+                processIDs.push_back(pe32.th32ProcessID);
+            }
+        } while (Process32Next(hProcessSnap, &pe32));
+    }
+    CloseHandle(hProcessSnap);
+    return processIDs;
+}
---
+++ SeewoCore/pch.h
@@ -0,0 +1,30 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+extern "C" {
+	//用于检查指定进程是否有窗口，如果窗口不可见则返回false
+	_declspec(dllexport) bool HasWindow(DWORD processID);
+	// 检查指定的进程是否在前台
+	_declspec(dllexport) bool IsProcessForeground(DWORD processId);
+	// 杀死进程的函数
+	_declspec(dllexport) bool KillProcess(DWORD processID);
+
+}
+
+// 遍历系统中符合指定名称的进程并返回它们的进程ID
+_declspec(dllexport) vector<DWORD> TraversalProcesses(const wchar_t* targetProcessName);
+
+#endif //PCH_H
